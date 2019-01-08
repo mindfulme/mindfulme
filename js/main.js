4379c178c5f9a0fe64b39a6eac280e8521710442
Rostislav Gogolauri 'mindfulme <alice.gogolauri@gmail.com>
2019-01-08 16:13:11 +0200

    fixed errors

--- js/main.js
+++ js/main.js
@@ -151,32 +151,6 @@
             $pswp = $('.pswp')[0],
             $folioItems = $('.item-folio');
 
-        // get items
-        $folioItems.each(function (i) {
-
-            var $folio = $(this),
-                $thumbLink = $folio.find('.thumb-link'),
-                $title = $folio.find('.item-folio__title'),
-                $caption = $folio.find('.item-folio__caption'),
-                $titleText = '<h4>' + $.trim($title.html()) + '</h4>',
-                $captionText = $.trim($caption.html()),
-                $href = $thumbLink.attr('href'),
-                $size = $thumbLink.data('size').split('x'),
-                $width = $size[0],
-                $height = $size[1];
-
-            var item = {
-                src: $href,
-                w: $width,
-                h: $height
-            }
-
-            if ($caption.length > 0) {
-                item.title = $.trim($titleText + $captionText);
-            }
-
-            items.push(item);
-        });
 
         // bind click event
         $folioItems.each(function (i) {
